75c984e1cb8e41f08fa536835428b7d9868286af
Mihir-21 <trambadiyamihir635@gmail.com>
2024-10-20 22:24:01 +0200

    make changes in book issuing page

--- ElibrarManagement/adminbookissuing.aspx.cs
+++ ElibrarManagement/adminbookissuing.aspx.cs
@@ -34,13 +34,63 @@ namespace ElibrarManagement
         // GO button
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            getNames();
         }
 
         // return Book
         protected void Button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+
+        // user defined functions
+        void getNames()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(getConnectionString()))
+                {
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT full_name FROM member_master_tb1 WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count >= 1)
+                    {
+                        TextBox3.Text = dt.Rows[0]["member_name"].ToString();
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('Wrong Book id')</script>");
+
+                    }
+
+
+                    SqlCommand cmd1 = new SqlCommand("SELECT book_name FROM book_master_tb1 WHERE book_id='" + TextBox2.Text.Trim() + "'", con);
+                    SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
+                    DataTable dt1 = new DataTable();
+                    da.Fill(dt1);
+
+                    if (dt1.Rows.Count >= 1)
+                    {
+                        TextBox4.Text = dt1.Rows[0]["book_name"].ToString();
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('Wrong Book id')</script>");
+
+                    }
+
+                    Response.Write("<script>alert('Name is SELECTED..')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
         }
     }
 }
