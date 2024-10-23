6ee62b868cb0cf0990528ae4f58743a70c0bfc16
Mihir-21 <trambadiyamihir635@gmail.com>
2024-10-23 13:00:04 +0200

    completed book issuing page

--- ElibrarManagement/adminbookissuing.aspx
+++ ElibrarManagement/adminbookissuing.aspx
@@ -1,6 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="adminbookissuing.aspx.cs" Inherits="ElibrarManagement.adminbookissuing" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script type="text/javascript">
+$(document).ready(function () {
+    $(".table").prepend($("<thead></thead>").append($(this).find("tr:first"))).dataTable();
+});
+    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
@@ -80,13 +85,13 @@
                         <div class="row">
 
                             <div class="col-md-6">
-                                <label>Start Date</label>
+                                <label>Issue Date</label>
                                 <div class="form-group">
                                     <asp:TextBox CssClass="form-control" ID="TextBox5" runat="server" TextMode="Date"></asp:TextBox>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <label>End Date</label>
+                                <label>Due Date</label>
                                 <div class="form-group">
                                     <div class="input-group">
                                         <asp:TextBox CssClass="form-control" ID="TextBox6" runat="server" TextMode="Date"></asp:TextBox>
@@ -143,16 +148,19 @@
 
                         <div class="row">
                             <div class="col">
-                                <asp:GridView class="table table-striped table-bordered" ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1">
+                                <asp:GridView class="table table-striped table-bordered" ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" OnRowDataBound="GridView1_RowDataBound">
                                     <Columns>
                                         <asp:BoundField DataField="member_id" HeaderText="Member ID" SortExpression="member_id" />
                                         <asp:BoundField DataField="member_name" HeaderText="Member Name" SortExpression="member_name" />
                                         <asp:BoundField DataField="book_id" HeaderText="Book ID" SortExpression="book_id" />
                                         <asp:BoundField DataField="book_name" HeaderText="Book Name" SortExpression="book_name" />
+                                        <%--<asp:DynamicField DataField="issue_date" HeaderText="Issue Date" />--%>
+                                        <asp:BoundField DataField="issue_date" HeaderText="Issue Date" SortExpression="issue_date" />
+
                                         <asp:BoundField DataField="due_date" HeaderText="Due Date" SortExpression="due_date" />
                                     </Columns>
                                 </asp:GridView>
-                                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:elibraryDBConnectionString3 %>" SelectCommand="SELECT * FROM [book_issue_tb1]"></asp:SqlDataSource>
+                                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:elibraryDBConnectionString2 %>" SelectCommand="SELECT * FROM [book_issue_tb1]"></asp:SqlDataSource>
                             </div>
                         </div>
                     </div>
--- ElibrarManagement/adminbookissuing.aspx.cs
+++ ElibrarManagement/adminbookissuing.aspx.cs
@@ -28,7 +28,23 @@ namespace ElibrarManagement
         // Issued Book
         protected void Button2_Click(object sender, EventArgs e)
         {
+            if (checkIfBookExist() && checkIfMemberExist())
+            {
+                if (checkIfIssueEntryExist())
+                {
+                    Response.Write("<script>alert('this Member Already has this Book.')</script>");
 
+                }
+                else
+                {
+                    insertIssuedBook();
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Member id and Book id dose not Exists.')</script>");
+
+            }
         }
 
         // GO button
@@ -40,11 +56,147 @@ namespace ElibrarManagement
         // return Book
         protected void Button3_Click(object sender, EventArgs e)
         {
-
+            returnBook();
         }
 
 
         // user defined functions
+
+
+        bool checkIfBookExist()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM book_master_tb1 WHERE book_id='" + TextBox2.Text.ToString() + "' AND current_stock > 0", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        bool checkIfMemberExist()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT full_name FROM member_master_tb1 WHERE member_id='" + TextBox1.Text.ToString() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        void insertIssuedBook()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(getConnectionString()))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO book_issue_tb1 (member_id , member_name, book_id, book_name, issue_date, due_date) VALUES (@member_id , @member_name, @book_id, @book_name, @issue_date, @due_date) ", con);
+                    cmd.Parameters.AddWithValue("@member_id", TextBox1.Text.ToString());
+                    cmd.Parameters.AddWithValue("@book_id", TextBox2.Text.ToString());
+                    cmd.Parameters.AddWithValue("@member_name", TextBox3.Text.ToString());
+                    cmd.Parameters.AddWithValue("@book_name", TextBox4.Text.ToString());
+                    cmd.Parameters.AddWithValue("@issue_date", TextBox5.Text.ToString());
+                    cmd.Parameters.AddWithValue("@due_date", TextBox6.Text.ToString());
+
+                    cmd.ExecuteNonQuery();
+
+
+                    cmd = new SqlCommand("UPDATE book_master_tb1 SET current_stock = current_stock - 1 WHERE book_id ='"+ TextBox2.Text.ToString() + "'", con);
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                    Response.Write("<script>alert('Book Issued..')</script>");
+                    
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        void returnBook()
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(getConnectionString()))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM book_issue_tb1 WHERE book_id='"+ TextBox2.Text.ToString() +"' AND member_id='"+ TextBox1.Text.ToString() +"'", con);
+                    int result = cmd.ExecuteNonQuery();
+
+                    if(result > 0)
+                    {
+                        cmd = new SqlCommand("UPDATE book_master_tb1 SET current_stock = current_stock+1 WHERE book_id='"+ TextBox2.Text.ToString() +"'", con);
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        Response.Write("<script>alert('Book Returned Successfully')</script>");
+                        GridView1.DataBind();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        bool checkIfIssueEntryExist()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM book_issue_tb1 WHERE member_id='" + TextBox1.Text.ToString() + "' AND book_id='"+ TextBox2.Text.ToString() +"'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
         void getNames()
         {
             try
@@ -53,38 +205,56 @@ namespace ElibrarManagement
                 {
                     con.Open();
 
-                    SqlCommand cmd = new SqlCommand("SELECT full_name FROM member_master_tb1 WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                    SqlCommand cmd = new SqlCommand("SELECT full_name FROM member_master_tb1 WHERE member_id='"+ TextBox1.Text.ToString() +"'", con);
                     SqlDataAdapter da = new SqlDataAdapter(cmd);
                     DataTable dt = new DataTable();
                     da.Fill(dt);
 
-                    if (dt.Rows.Count >= 1)
+                    if(dt.Rows.Count >= 1 )
                     {
-                        TextBox3.Text = dt.Rows[0]["member_name"].ToString();
+                        TextBox3.Text = dt.Rows[0]["full_name"].ToString();
                     }
                     else
                     {
-                        Response.Write("<script>alert('Wrong Book id')</script>");
-
+                            Response.Write("<script>alert('Wrong member id')</script>");
                     }
 
+                    cmd = new SqlCommand("SELECT book_name FROM book_master_tb1 WHERE book_id='"+ TextBox2.Text.ToString() +"'", con);
+                    da = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    da.Fill(dt);
 
-                    SqlCommand cmd1 = new SqlCommand("SELECT book_name FROM book_master_tb1 WHERE book_id='" + TextBox2.Text.Trim() + "'", con);
-                    SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
-                    DataTable dt1 = new DataTable();
-                    da.Fill(dt1);
-
-                    if (dt1.Rows.Count >= 1)
+                    if(dt.Rows.Count >= 1 )
                     {
-                        TextBox4.Text = dt1.Rows[0]["book_name"].ToString();
+                        TextBox4.Text = dt.Rows[0]["book_name"].ToString();
                     }
                     else
                     {
-                        Response.Write("<script>alert('Wrong Book id')</script>");
+                           Response.Write("<script>alert('Wrong Book id')</script>");
 
                     }
 
-                    Response.Write("<script>alert('Name is SELECTED..')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            try
+            {
+                if(e.Row.RowType == DataControlRowType.DataRow)
+                {
+                    DateTime dt = Convert.ToDateTime(e.Row.Cells[5].Text);
+                    DateTime today = DateTime.Today;
+
+                    if(today > dt)
+                    {
+                        e.Row.BackColor = System.Drawing.Color.PaleVioletRed;
+                    }
                 }
             }
             catch (Exception ex)
